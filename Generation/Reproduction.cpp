8110179e8ff581fb4d00fa64e0d7ed22fd9f685a
Roy Claudia <1330655@decinfo.cvm>
2018-12-14 14:16:37 -0500

    allo

--- Generation/Reproduction.cpp
+++ Generation/Reproduction.cpp
@@ -1,5 +1,6 @@
 #include "Reproduction.h"
 #include "Transactions.h"
+#include "Random.h"
 Reproduction::Reproduction()
 {
 }
@@ -11,7 +12,7 @@ Reproduction::~Reproduction()
 
 Reproduction::StateRep Reproduction::createChild(StateRep & state)
 {
-
+	
 	//select, generatechild, mutate
 	switch (state) {
 	case StateRep::select:
@@ -19,6 +20,14 @@ Reproduction::StateRep Reproduction::createChild(StateRep & state)
 			return nextState(state);
 		}
 		else {
+
+			//Je choisie mes 2 prant dans le vecteur de forme je dois en choisir random 2
+			int randomParentIndex1 = Random::getInstance().uniformRandomize(1, 23); //23est un nombre bidon pour le nombre de forme que nous allons avoir setter
+			int randomParentIndex2 = Random::getInstance().uniformRandomize(1, 23); //23est un nombre bidon pour le nombre de forme que nous allons avoir setter
+
+			//parent1= ArrayForme[randomParentIndex1] ;
+			//parent1= ArrayForme[randomParentIndex1] ;
+
 			return state;
 		}
 		break;
@@ -27,6 +36,13 @@ Reproduction::StateRep Reproduction::createChild(StateRep & state)
 			return nextState(state);
 		}
 		else {
+			//Je prend mes 2 parents
+			//Je choisie le randomize de la coupure
+			int indexSplit = Random::getInstance().uniformRandomize(1, 36);
+			int mask{ (int)pow(2,indexSplit) - 1 };
+			//Je realise l'enfant
+			binaireEnfant = parent1 & mask | parent2 & ~mask;
+
 			return state;
 		}
 		break;
@@ -35,10 +51,18 @@ Reproduction::StateRep Reproduction::createChild(StateRep & state)
 			return StateRep::select;
 		}
 		else {
+			//Es ce que je fait un mutant ou pas
+
+			if (int a = Random::getInstance().uniformRandomize(1, 100) <= percentageMutate)
+			{
+				
+			}
+
 			return state;
 		}
 		break;
 	}
+
 }
 Reproduction::StateRep Reproduction::nextState(StateRep & state) {
 	return (StateRep)((int)state + 1);
