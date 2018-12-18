16a50632d5e03050a4a9e3a08e450a19e1d36f20
Doutre-Guay Ludovic <1349519@decinfo.cvm>
2018-12-18 08:29:30 -0500

    f

--- Generation/Population.cpp
+++ Generation/Population.cpp
@@ -1,12 +1,37 @@
 #include "Population.h"
 
 
-
-Population::Population()
+Population::Population(ConsoleColor::Text color):
+mColor{color}
 {
-}
+	if (Area::getInstance().shape_g == "cercle") {
+		for (int i = 0; i < NBPOPULATION; ++i) {
+			mSolutions[i] = new Cercle();
+			mSolutions[i]->randomize();
 
+		}
+	}
+	else if (Area::getInstance().shape_g == "triangle") {
+		for (int i = 0; i < NBPOPULATION; ++i) {
+			//mSolutions[i] = new Triangle();
+			//mSolutions[i]->randomize();
+
+		}
+	}
+	else {
+		for (int i = 0; i < NBPOPULATION; ++i) {
+			//mSolutions[i] = new Rectangle();
+			//mSolutions[i]->randomize();
+
+		}
+	}
+	
+}
 
 Population::~Population()
 {
+}
+
+void Population::draw(ConsoleImage & image) {
+	Area::getInstance().drawShape(mSolutions, NBPOPULATION, mColor);
 }
--- Generation/Population.h
+++ Generation/Population.h
@@ -1,8 +1,21 @@
-#pragma once
+#ifndef POPULATIONS_H
+#define POPULATIONS_H
+
+#include "Shape2D.h"
+#include "Area.h"
+#include "Console\ConsoleColor.h"
+
 class Population
 {
 public:
-	Population();
+	Population(ConsoleColor::Text color);
 	~Population();
+
+	void draw(ConsoleImage & image);
+
+private :
+	ConsoleColor::Text mColor;
+	Shape2D *mSolutions[NBPOPULATION];
 };
 
+#endif
